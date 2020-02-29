176fd7b2d7fea9cca605678666a15b70c04e0ee0
atoran <ahmetturesk@gmail.com>
2020-02-29 14:36:10 -0500

    add CancelReservation.java

---
+++ src/test/CancelReservation.java
@@ -0,0 +1,7 @@
+package test;
+
+public class CancelReservation {
+    public static void main(String[] args) {
+        System.out.println("This is written from Intellijj and this is thr first line of code");
+    }
+}
